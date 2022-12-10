e695da57de6645a55466312035282fe32b4f79d4
Faizan <faizansohail077@gmail.com>
2022-12-10 23:49:10 +0500

    added header and footer

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Footer, Navbar } from '../components'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="container mx-auto font-sans">
+      <Navbar />
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <Footer/>
+    </div>
+  )
 }
 
 export default MyApp
